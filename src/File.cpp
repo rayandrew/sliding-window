c7abe62e9ba7634e23433e4099bd60e85b7e7ab9
Winarto <yowinarto@gmail.com>
2017-10-16 22:55:37 +0700

    Read file

---
+++ src/File.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+
+std::ifstream::pos_type filesize(const char* filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    return in.tellg(); 
+}
+
+unsigned char* readFile(const char* filename, int nbyte)
+{
+	char* fileContent;
+	ifstream myfile(filename, ios::in | ios::binary | ios::ate);
+	if (myfile.is_open()) {
+		fileContent = new char[nbyte];		
+		myfile.seekg(0, ios::beg);
+		myfile.read(fileContent, nbyte);
+		myfile.close();
+	}
+	return (unsigned char*)fileContent;
+	//Can cause memory leak
+}
